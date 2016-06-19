ca42783b41fa7016f1c71a6be7c49ea3d7e66ea4
jackalwei <jackal8125@gmail.com>
2016-06-19 12:19:59 +0800

    Basic CRUD feature use match routes

---
+++ app/models/event.rb
@@ -0,0 +1,5 @@
+class Event < ActiveRecord::Base
+  validates_presence_of :name
+  #validates :name, presence: true
+  #這是新版Document裡提供的寫法
+end
--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  match ':controller(/:action(/:id(.:format)))', :via => :all
 end
