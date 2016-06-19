4d8ecb51e9be877e649cec8975534eb0a2e0bfa7
jackalwei <jackal8125@gmail.com>
2016-06-19 15:47:36 +0800

    add Respond_to feature to show other format

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,30 @@ class EventsController < ApplicationController
   # GET /events
   def index
     @events = Event.page(params[:page]).per(5)
+
+    #提供支援別的檔案格式
+    respond_to do |format|
+      format.html #index.html.erb
+      format.xml {
+        render :xml => @events.to_xml
+      }
+      format.json {
+        render :json => @events.to_json
+      }
+      format.atom {
+        @feed_title = "My event list"
+      }
+    end
   end
 
   # GET /events/:id
   def show
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   # GET /events/new
