5bf455e9a27e7ab8788671da240c0abe4d207b49
jackalwei <jackal8125@gmail.com>
2016-06-21 20:56:45 +0800

    add column(role) into users table to implement Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621120124) do
+ActiveRecord::Schema.define(version: 20160621124617) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160621120124) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
