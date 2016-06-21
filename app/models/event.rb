aeb4846233843c87550e40a22088d0e458d906f3
jackalwei <jackal8125@gmail.com>
2016-06-21 16:36:16 +0800

    add many to many use check_box

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,7 @@ class Event < ActiveRecord::Base
   belongs_to :category
 
   delegate :name, :to => :category, :prefix => true, :allow_nil => true
+
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
 end
