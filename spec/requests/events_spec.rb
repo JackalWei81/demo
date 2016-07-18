efe96d2e11264ee6e3f10b70601fad78c9c84e0a
jackalwei <jackal8125@gmail.com>
2016-07-18 15:15:23 +0800

    add Request API event show rspec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 RSpec.describe "API_V1::Events", :type => :request do
 
-  example "should get events list" do
-    get "/api/v1/events"
-    expect(response).to have_http_status(200)
-    expect( JSON.parse(response.body) ).to eq( [] )
+  describe "GET /api/v1/events/:id" do
+    it "should be return events json" do
+      user = User.create!( email: "jackal@gmail.com", password: "123456")
+
+      get v1_event_path(2)
+
+      data = {
+        "event" => []
+      }
+
+      expect(response).to have_http_status(200)
+
+      parsed_json = JSON.parse( response.body )
+      expect( parsed_json ).to eq(data)
+    end
   end
 end
